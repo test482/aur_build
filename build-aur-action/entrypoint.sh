d43eb251c4b3b8fb2e150096e0e3bdcbe4789529
Eliot <eliotjoking@gmail.com>
2023-01-12 10:38:55 +0800

    fix ffmpeg-obs build error

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -7,13 +7,15 @@ echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 chmod -R a+rw .
 
 cat << EOM >> /etc/pacman.conf
+[multilib]
+Include = /etc/pacman.d/mirrorlist
 [archlinuxcn]
 Server = https://repo.archlinuxcn.org/x86_64
 EOM
 
 pacman-key --init
 pacman -Sy --noconfirm && pacman -S --noconfirm archlinuxcn-keyring
-pacman -Syu --noconfirm yay
+pacman -Syu --noconfirm --needed base-devel yay
 if [ ! -z "$INPUT_PREINSTALLPKGS" ]; then
     yay -Syu --noconfirm "$INPUT_PREINSTALLPKGS"
 fi
